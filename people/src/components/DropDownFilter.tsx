0280ffd42cf2341eba37aeb0a2e1161fa847f7bf
TudorMaiereanu <tudormaiereanu@gmail.com>
2019-04-08 11:53:33 +0200

    implemented first version of dropdown

--- people/src/components/DropDownFilter.tsx
+++ people/src/components/DropDownFilter.tsx
@@ -1,14 +1,42 @@
 import React, { Component } from "react";
+import Select from "react-select";
 
 type Props = {
     elements: string[];
 }
 
-class DropDownFilter extends Component<Props, {}> {
+
+class DropDownFilter extends Component<Props, any> {
+  constructor(props: Props){
+    super(props);
+    
+    this.state = { 
+      elements: this.props.elements,
+      options: null,
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange = (options: any) => {
+    this.setState({ options });
+  }
+
   public render() {
+    const { options } = this.state;
+    const { elements } = this.state;
+
     return (
         <div>
-            dropdown filter
+            <Select
+              isMulti
+              value = {options}
+              onChange={this.handleChange}
+              options={elements.map((option: string) => ({
+                label: option,
+                value: option,
+            }))}
+            />
         </div>
     );
   }
